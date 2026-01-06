74e514f17f84a8f611568d46339131997afaf621
Nawaraj Shah <shah.nawaraj.ns@gmail.com>
2026-01-06 11:53:12 +0545

    Method and parameters

---
+++ MethodAndParameters.cs
@@ -0,0 +1,23 @@
+﻿using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace MethodAndParameters
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine(Add(5, 10));
+            Console.WriteLine(Add(5.5, 2.5));
+        }
+
+        static int Add(int a, int b)
+        {
+            return a + b;
+        }
+
+        static double Add(double a, double b)
+        {
+            return a + b;
+        }
+    }
+}
